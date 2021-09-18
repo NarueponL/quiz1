951ff28f59bec45603022fb6ada64a5f5acd43f4
Naruepon <78534947+NarueponL@users.noreply.github.com>
2021-09-18 10:38:54 +0700

    quizz

---
+++ FlowerInformation.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+public class FlowerInformation
+{
+	public string plantId;
+	public string plantName;
+	public string plantDescription;
+	public int amount;
+	public string height;
+	public string circumference;
+
+	public FlowerInformation(string plantId ,string plantName,string plantDescription,int amount ,string height,string circumference)
+	{
+		this.plantId = plantId;
+		this.plantName = plantName;
+		this.plantDescription = plantDescription;
+		this.amount = amount;
+		this.height = height;
+		this.circumference = circumference;
+	}
+}
---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace quiz
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int totalRose=0;
+            int totalSunFlower=0;
+
+            GetAmountPlant(ref totalRose, ref totalSunFlower);
+            GetFlowerInformation(totalRose);
+            
+
+        }
+
+        static void GetAmountPlant(ref int totalrose , ref int totalsunflower)
+        {
+            
+            Console.Write("Input Total Rose : ");
+            totalrose = int.Parse(Console.ReadLine());
+            Console.Write("Input Total Sun Flower : ");
+            totalsunflower = int.Parse(Console.ReadLine());
+        }
+        static void GetFlowerInformation(int total)
+        {
+            for(int i = 1; i <= total; i++)
+            {
+                
+
+            }
+        }
+    }
+}
